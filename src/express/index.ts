5032e008d324668904addc5a1ed6ac800b614f8f
Eunbin <qpmz1212@naver.com>
2020-11-26 19:11:04 +0900

    feat: express용 errorHandler 구현 - express 에서 사용할 middleware errorHandler 구현

--- src/express/index.ts
+++ src/express/index.ts
@@ -1,1 +1,21 @@
-export { init, errorHandler } from './expressInit';
+import ExpressClient from './client';
+import { Options } from '../type';
+
+interface globalSantry extends NodeJS.Global {
+  santry?: {
+    options: Options;
+  };
+}
+
+const expressSantry: globalSantry = global;
+
+export const getExpressSantry = () => {
+  return expressSantry;
+};
+
+export const init = (options) => {
+  const client = ExpressClient(options);
+  expressSantry.santry.options = client;
+};
+
+export { errorHandler } from './errorHandler';
