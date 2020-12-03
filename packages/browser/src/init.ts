80fa7853d82adc4684a77e810a5f84473c892501
Hera <maong0927@gmail.com>
2020-12-03 15:25:34 +0900

    fix     : 전역 객체 위치 변경
    1. 기존 : 전역 객체의 위치를 함수 밖에 두었으나
    2. 현재 : 동작이 되지 않아 함수 안으로 변경

--- packages/browser/src/init.ts
+++ packages/browser/src/init.ts
@@ -1,26 +1,28 @@
 import { getGlobalObject } from '@santry/utils';
-import { Options, Message, Dsn, Title, Context } from '@santry/types';
+import { Options, Message, Dsn, ContextTitle, Context } from '@santry/types';
 import { BrowserSantry } from './browserSantry';
 import { initWithClass } from '@santry/core';
-
-const { santry } = getGlobalObject<Window>();
 
 export const init = (dsn: Dsn, options: Options): void => {
   initWithClass(BrowserSantry, dsn, options);
 };
 
 export const captureError = (error: Error): void => {
+  const { santry } = getGlobalObject<Window>();
   santry.hub.captureError(error);
 };
 
 export const captureMessage = (message: Message): void => {
+  const { santry } = getGlobalObject<Window>();
   santry.hub.captureMessage(message);
 };
 
-export const setContext = (title: Title, context: Context): void => {
+export const setContext = (title: ContextTitle, context: Context): void => {
+  const { santry } = getGlobalObject<Window>();
   santry.hub.setContext(title, context);
 };
 
 export const setLevel = (level: string): void => {
+  const { santry } = getGlobalObject<Window>();
   santry.hub.setLevel(level);
 };
--- packages/node/src/init.ts
+++ packages/node/src/init.ts
@@ -1,26 +1,28 @@
 import { getGlobalObject } from '@santry/utils';
 import { NodeSantry } from './nodeSantry';
-import { Options, Message, Dsn, Title, Context } from '@santry/types';
+import { Options, Message, Dsn, ContextTitle, Context } from '@santry/types';
 import { initWithClass } from '@santry/core';
-
-const { santry } = getGlobalObject<NodeJS.Global>();
 
 export const init = (dsn: Dsn, options: Options): void => {
   initWithClass(NodeSantry, dsn, options);
 };
 
 export const captureError = (error: Error): void => {
+  const { santry } = getGlobalObject<NodeJS.Global>();
   santry.hub.captureError(error);
 };
 
 export const captureMessage = (message: Message): void => {
+  const { santry } = getGlobalObject<NodeJS.Global>();
   santry.hub.captureMessage(message);
 };
 
-export const setContext = (title: Title, context: Context): void => {
+export const setContext = (title: ContextTitle, context: Context): void => {
+  const { santry } = getGlobalObject<NodeJS.Global>();
   santry.hub.setContext(title, context);
 };
 
 export const setLevel = (level: string): void => {
+  const { santry } = getGlobalObject<NodeJS.Global>();
   santry.hub.setLevel(level);
 };
