7f4139b08230c4707ccd39ee7320a991f69bbd82
Hera <maong0927@gmail.com>
2020-12-03 11:55:56 +0900

    refactor: 공통 변수 전역으로 이동
    1. 공통으로 쓰는 globalObject 변수 전역으로 이동

--- packages/browser/src/init.ts
+++ packages/browser/src/init.ts
@@ -3,26 +3,24 @@ import { Options } from '@santry/types';
 import { BrowserSantry } from './browserSantry';
 import { initWithClass } from '@santry/core';
 
+const { santry } = getGlobalObject<Window>();
+
 export const init = (dsn: string, options: Options) => {
   initWithClass(BrowserSantry, dsn, options);
 };
 
 export const captureError = (error: Error): void => {
-  const { santry } = getGlobalObject<Window>();
   santry.hub.captureError(error);
 };
 
 export const captureMessage = (message: string): void => {
-  const { santry } = getGlobalObject<Window>();
   santry.hub.captureMessage(message);
 };
 
 export const setContext = (title: string, contents: any): void => {
-  const { santry } = getGlobalObject<Window>();
   santry.hub.setContext(title, contents);
 };
 
 export const setLevel = (level: string): void => {
-  const { santry } = getGlobalObject<Window>();
   santry.hub.setLevel(level);
 };
--- packages/node/src/init.ts
+++ packages/node/src/init.ts
@@ -3,26 +3,24 @@ import { NodeSantry } from './nodeSantry';
 import { Options } from '@santry/types';
 import { initWithClass } from '@santry/core';
 
+const { santry } = getGlobalObject<NodeJS.Global>();
+
 export const init = (dsn: string, options: Options): void => {
   initWithClass(NodeSantry, dsn, options);
 };
 
 export const captureError = (error: Error): void => {
-  const { santry } = getGlobalObject<NodeJS.Global>();
   santry.hub.captureError(error);
 };
 
 export const captureMessage = (message: string): void => {
-  const { santry } = getGlobalObject<NodeJS.Global>();
   santry.hub.captureMessage(message);
 };
 
 export const setContext = (title: string, contents: any): void => {
-  const { santry } = getGlobalObject<NodeJS.Global>();
   santry.hub.setContext(title, contents);
 };
 
 export const setLevel = (level: string): void => {
-  const { santry } = getGlobalObject<NodeJS.Global>();
   santry.hub.setLevel(level);
 };
