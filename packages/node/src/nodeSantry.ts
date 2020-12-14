6de30c0d4bea9b3da0bc112f8c11bdbff9558ad8
Hera <maong0927@gmail.com>
2020-12-14 16:07:45 +0900

    feat    : level 주입 형식 변경으로 로직 변경
    1. 기존 setEvent 에서 레벨을 설정해서 넣어줬으나, parameter 로 넣어주는 형식으로 변경

--- packages/node/src/nodeSantry.ts
+++ packages/node/src/nodeSantry.ts
@@ -1,4 +1,4 @@
-import { getNodeEtcInfo } from '@santry/utils';
+import { getNodeEtcInfo, getLevel } from '@santry/utils';
 import { BaseSantry } from '@santry/core';
 import { Dsn, Message, Options } from '@santry/types';
 import packages from '../package.json';
@@ -10,33 +10,41 @@ export class NodeSantry extends BaseSantry {
     this.sdk = { name: packages.name, version: packages.version };
   }
 
-  public captureError(error: Error): void {
-    this.createEvent(error, getNodeEtcInfo());
+  public captureError(error: Error, level: string): void {
+    this.createEvent(
+      error,
+      getLevel({ isError: true, level }),
+      getNodeEtcInfo(),
+    );
   }
 
-  public captureMessage(message: Message): void {
-    this.createEvent(message, getNodeEtcInfo());
+  public captureMessage(message: Message, level: string): void {
+    this.createEvent(
+      message,
+      getLevel({ isError: false, level }),
+      getNodeEtcInfo(),
+    );
   }
 
   public onUncaughtException(): void {
     process.on('uncaughtException', (error: Error) => {
-      const options = this.getOptions();
-      const level = options.uncaughtExceptionLevel
-        ? options.uncaughtExceptionLevel
-        : 'error';
-      this.setLevel(level);
-      this.createEvent(error);
+      const level = this.options.uncaughtExceptionLevel;
+      this.createEvent(
+        error,
+        getLevel({ isError: true, level }),
+        getNodeEtcInfo(),
+      );
     });
     return;
   }
   public onUnhandledRejection(): void {
     process.on('unhandledRejection', (reason: Error, promise: Promise<any>) => {
-      const options = this.getOptions();
-      const level = options.unhandledRejectionLevel
-        ? options.unhandledRejectionLevel
-        : 'error';
-      this.setLevel(level);
-      this.createEvent(reason);
+      const level = this.options.unhandledRejectionLevel;
+      this.createEvent(
+        reason,
+        getLevel({ isError: true, level }),
+        getNodeEtcInfo(),
+      );
     });
     return;
   }
--- packages/node/src/sdk.ts
+++ packages/node/src/sdk.ts
@@ -7,22 +7,17 @@ export const init = (dsn: Dsn, options: Options): void => {
   initWithClass(NodeSantry, dsn, options);
 };
 
-export const captureError = (error: Error): void => {
+export const captureError = (error: Error, level: string): void => {
   const { santry } = getGlobalObject<NodeJS.Global>();
-  santry.hub.captureError(error);
+  santry.hub.captureError(error, level);
 };
 
-export const captureMessage = (message: Message): void => {
+export const captureMessage = (message: Message, level: string): void => {
   const { santry } = getGlobalObject<NodeJS.Global>();
-  santry.hub.captureMessage(message);
+  santry.hub.captureMessage(message, level);
 };
 
 export const setContext = (title: ContextTitle, context: Context): void => {
   const { santry } = getGlobalObject<NodeJS.Global>();
   santry.hub.setContext(title, context);
-};
-
-export const setLevel = (level: string): void => {
-  const { santry } = getGlobalObject<NodeJS.Global>();
-  santry.hub.setLevel(level);
 };
