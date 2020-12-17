011ca60053187d78bb8af6ea423cdf3411215005
Hera <maong0927@gmail.com>
2020-12-17 20:03:09 +0900

    chore   : polyfill 적용

--- examples/node.example.js
+++ examples/node.example.js
@@ -1,6 +1,7 @@
 const { init, captureError, captureMessage } = require('@santry/node');
 
-const dsn = '[token]@[url]';
+const dsn =
+  'eyJhbGciOiJIUzI1NiJ9.NWZkYjMyYTVjOGU3YTUzYjMwY2VlZTUx.aWHr0JTIjOtHQ4LVQCOUl5R2vmVn89StPhOE0WDevjY@118.67.129.120:3000/sdk/event';
 
 init(dsn, {
   traceSampleRate: 1,
